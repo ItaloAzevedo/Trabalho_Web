fb05d8ce143920d52b23329265b10fb245edf987
Thalita <thalitaednelly15@gmail.com>
2018-12-14 18:47:26 -0200

    _ no nome dos arquivos para funcionar

--- app/Http/Controllers/Especialidade.php
+++ app/Http/Controllers/Especialidade.php
@@ -8,11 +8,11 @@ class Especialidade extends Controller
 {
     public function listar(){
       
-    return view('especialidade.listar');
+    return view('especialidade_listar');
   }
     
     public function adicionar(){
       
-    return view('especialidade.adicionar');
+    return view('especialidade_adicionar');
   }
 }
